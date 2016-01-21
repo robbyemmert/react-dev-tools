ad4a03d366aae11f43fb3a885d44d80ae09276be
Robby Emmert <robby@robbyemmert.com>
2016-01-21 07:25:17 -0500

    reactified things and cleaned up gitignore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-console.log('Hello Thar');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+    document.getElementById('test-element')
+);
+
+var person = 'Christine';
+console.log(`Hello ${person}`);
 
 export class SomeClass {
     constructor() {
